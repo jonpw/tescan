0e401cbeea6fb27b00f5a274d73e047e061c6f52
jon <dooberx@gmail.com>
2020-01-18 11:32:46 +1000

    wip

--- canmqtt.py
+++ canmqtt.py
@@ -1,33 +1,36 @@
 import can
 import cantools
-from influxdb import InfluxDBClient
-from influxdb import exceptions as ifxexcept
+import paho.mqtt.client as mqtt
 import time
 import traceback
 
 bus = can.Bus(bustype='socketcan', channel='vcan0', bitrate=500000, receive_own_messages=True)
 db = cantools.database.load_file('Model3CAN.dbc')
+vehicle = 'maximus'
 
 while True:
         try:
-                client = InfluxDBClient('701.insi.dev', 38086, 'maximus', 'campari', 'test')
-                client.create_database('test')
+        		client = mqtt.Client()
+        		client.on_connect = on_connect
+        		client.on_message = on_message
+        		client.connect("701.insi.dev", 1883, 60)
+        		client.loop_start()
                 break
         except:
                 time.sleep(10)
 
-vehicle = 'maximus'
 
 one_json = lambda message:{ "measurement": vehicle, "tags": {"message": message}, "time": int(time.time_ns()/1000), "fields": {}}
-
+ilp = lambda message, time, signals:str(vehicle)+','+','.join([str(key)+'='+str(val) for ])
 while True:
         message = bus.recv()
         decoded = db.decode_message(message.arbitration_id, message.data)
         json_message = one_json(db.get_message_by_frame_id(message.arbitration_id).name)
         json_message["time"] = int(message.timestamp*1000)
         json_message["fields"].update(decoded)
+
         json_body = [json_message,]
-        #print(str(json_body))
+        #print(str(json_body))d
         try:
                 client.write_points(json_body)
         except ifxexcept.InfluxDBClientError:
--- cantest.py
+++ cantest.py
@@ -1,25 +1,46 @@
+import traceback
 import can
 import cantools
 from influxdb import InfluxDBClient
 import time
 import random
 
-bus = can.interface.Bus(bustype='socketcan', channel='vcan0', bitrate=500000)
+bus = can.Bus(bustype='socketcan', channel='vcan0', bitrate=500000, receive_own_messages=True)
 db = cantools.database.load_file('Model3CAN.dbc')
 messages = db.messages
 
 while True:
-	message = messages[random.randint(0,len(messages))]
-	sigdata = {}
-	for signal in message.signals:
-		if signal.is_float:
-			sigdata[signal.name] = random.randint(signal.minimum, signal.maximum)
-		else:
-			sigdata[signal.name] = random.randint(signal.minimum, signal.maximum)
-	encoded = message.encode(sigdata)
-	new_message = can.Message(arbitration_id=message.frame_id, data=encoded)
-	bus.send(new_message)
-	time.sleep(0.1)
-
-
-
+        time.sleep(0.1)
+        message = random.choice(messages)
+        #message = messages[0]
+        sigdata = {}
+        for signal in message.signals:
+                #print(str(signal))
+                try:
+                        if signal.minimum == None:
+                                min=0
+                        else:
+                                min=signal.minimum
+                        if signal.maximum == None:
+                                max=((2**signal.length)-1)*signal.scale
+                        else:
+                                max=signal.maximum
+                        if signal.is_float:
+                                sigdata[signal.name] = random.randint(int(min), int(max))
+                        else:
+                                sigdata[signal.name] = random.randint(int(min), int(max))
+                except Exception:
+                        print('Error creating :'+str(signal))
+                        traceback.print_exc()
+                        continue
+        #print(str(sigdata))
+        try:
+                encoded = message.encode(sigdata)
+        except:
+                print('Error encoding :'+str(sigdata))
+                print(message.signals)
+                traceback.print_exc()
+                continue
+        new_message = can.Message(arbitration_id=message.frame_id, data=encoded)
+        bus.send(new_message)
+        #print(str(new_message))
