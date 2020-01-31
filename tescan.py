4960a525ff465f48dc911012c3c368b2063b11b0
jon <dooberx@gmail.com>
2020-02-01 06:31:03 +1000

    wip for deployment

--- tescan.py
+++ tescan.py
@@ -1,8 +1,27 @@
 import can
+import sys, getopt
 from influx import InfluxWriter
 from mqttlistener import MqttWriter
 
-bus = can.Bus(bustype='socketcan', channel='vcan0', bitrate=500000, receive_own_messages=True, listen_only=True)
+candev='vcan0'
+printonly=False
+
+try:
+    opts, args = getopt.getopt(argv,"hb:p")
+except getopt.GetoptError:
+    print('tescan.py -b <can device>')
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == '-h':
+        print('tescan.py -b <can device>')
+        sys.exit()
+    elif opt in ("-b"):
+        candev = arg
+    elif opt in ("-p"):
+        printonly=True
+print('Using bus '+candev)
+
+bus = can.Bus(bustype='socketcan', channel=candev, bitrate=500000, receive_own_messages=True, listen_only=True)
 
 hostname = '701.insi.dev'
 vehicle = 'maximus'
@@ -11,14 +30,16 @@ password = 'campari'
 database_file='model3dbc/Model3CAN.dbc'
 sqlitefile = '/var/lib/tescan/canbus.sqlite'
 
-influxwriter = InfluxWriter(hostname, database=vehicle, measurement_name=vehicle, user=user, password=password, database_file=database_file)
 printer = can.Printer()
-sqlitewriter = can.SqliteWriter(sqlitefile, table_name=vehicle)
-mqttwriter = MqttWriter(hostname, vehicle=vehicle, user=user, password=password, database_file=database_file)
+if not printonly:
+    influxwriter = InfluxWriter(hostname, database=vehicle, measurement_name=vehicle, user=user, password=password, database_file=database_file)
+    sqlitewriter = can.SqliteWriter(sqlitefile, table_name=vehicle)
+    mqttwriter = MqttWriter(hostname, vehicle=vehicle, user=user, password=password, database_file=database_file)
 
 while True:
     message = bus.recv()
     printer(message)
-    sqlitewriter(message)
-    influxwriter(message)
-    mqttwriter(message)
+    if not printonly:
+        sqlitewriter(message)
+        influxwriter(message)
+        mqttwriter(message)
--- tescan.sh
+++ tescan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd /var/lib/tescan
 git pull
 git submodule update
-python3 tescan.py
+sudo ip link set can0 type can bitrate 500000 listen-only yes && sudo ip link set up can0 && python3 tescan.py
