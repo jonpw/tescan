66383151a7300a314453b025f467aea65241a76a
jon <dooberx@gmail.com>
2020-02-02 06:01:46 +1000

    Update tescan.sh

--- tescan.sh
+++ tescan.sh
@@ -5,8 +5,10 @@ git pull
 git submodule update
 while [ 1 ]
 do
+	touch /tmp/tescan.start
 	sudo ip link set can0 type can bitrate 500000 listen-only on && \
 	sudo ip link set up can0 && \
+	touch /tmp/tescan.run &&
 	python3 tescan.py -b can0
 	sleep 60
 done
