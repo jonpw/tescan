0fa52198f3279df875d53ad7e67d4dde5d6a3231
jon <dooberx@gmail.com>
2020-02-03 06:00:44 +1000

    debug by wifi name

--- tescan.sh
+++ tescan.sh
@@ -6,10 +6,23 @@ git submodule update
 chmod 777 tescan.sh
 while [ 1 ]
 do
+	myssid=$(iwgetid -r)
+	candev=can0
+	if [ myssid == "Telstra565C60" ]
+	then
+		candev=vcan0
+	elif [ myssid == "jnet" ]
+	then
+		candev=can0
+	elif [ myssid == "" ]
+	then
+		sleep 10
+		continue
+	fi
 	touch /tmp/tescan.start
-	sudo ip link set can0 type can bitrate 500000 listen-only on && \
-	sudo ip link set up can0 && \
+	sudo ip link set ${candev} type can bitrate 500000 listen-only on && \
+	sudo ip link set up ${candev} && \
 	touch /tmp/tescan.run &&
-	python3 tescan.py -b can0
+	python3 tescan.py -b ${candev}
 	sleep 60
 done
