a39bfb254671a77f6298d462b6120b9f52ab4eec
Jon Williams <jon@intellidesign.com.au>
2020-02-19 14:52:28 +1000

    add intelli ssid support

--- tescan.sh
+++ tescan.sh
@@ -24,6 +24,13 @@ do
 		sudo ip link set ${candev} type can bitrate 500000 listen-only on
 		sudo ip link set up ${candev}
 		options="-d"
+	elif [ "${myssid}" == "intelli" ]
+	then
+		candev=can0
+		sudo ip link set down ${candev}
+		sudo ip link set ${candev} type can bitrate 500000 listen-only on
+		sudo ip link set up ${candev}
+		options="-d"
 	elif [ "${myssid}" == "" ]
 	then
 		sleep 10
